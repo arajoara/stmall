f230832cbc5946685b85ca05d4a4845411fe8c7f
arajoara <whdkfk1029@hanmail.net>
2025-01-22 15:26:43 +0900

    commit

---
+++ order/src/main/java/stmall/domain/OrderMgmt.java
@@ -0,0 +1,74 @@
+package stmall.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import lombok.Data;
+import stmall.OrderApplication;
+import stmall.domain.OrderModified;
+import stmall.domain.OrderPlaced;
+
+@Entity
+@Table(name = "OrderMgmt_table")
+@Data
+//<<< DDD / Aggregate Root
+public class OrderMgmt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String userId;
+
+    private String productName;
+
+    private Integer cnt;
+
+    private String order;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+        OrderModified orderModified = new OrderModified(this);
+        orderModified.publishAfterCommit();
+    }
+
+    public static OrderMgmtRepository repository() {
+        OrderMgmtRepository orderMgmtRepository = OrderApplication.applicationContext.getBean(
+            OrderMgmtRepository.class
+        );
+        return orderMgmtRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void updateStatus(DeliveryStarted deliveryStarted) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        OrderMgmt orderMgmt = new OrderMgmt();
+        repository().save(orderMgmt);
+
+        */
+
+        /** Example 2:  finding and process
+        
+
+        repository().findById(deliveryStarted.get???()).ifPresent(orderMgmt->{
+            
+            orderMgmt // do something
+            repository().save(orderMgmt);
+
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
---
+++ product/src/main/java/stmall/domain/Soldout.java
@@ -0,0 +1,24 @@
+package stmall.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import stmall.domain.*;
+import stmall.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class Soldout extends AbstractEvent {
+
+    private Long id;
+
+    public Soldout(Inventory aggregate) {
+        super(aggregate);
+    }
+
+    public Soldout() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
